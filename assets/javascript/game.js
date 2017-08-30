ea44fbaa339a1ee2b172048457c7f373eab51bb6
egcruz3 <egcruz3@yahoo.com>
2017-08-30 15:40:54 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,46 @@
-     // Make our variables global to the runtime of our application
+$(document).ready(function(){
+
+// Make our variables global to the runtime of our application
 //      var redGem;
 //      var whiteGem;
 //      var blueGem;
 //      var ltblueGem;
-        var
-        
-      function startGame() {
-              
-//  DO THIS FOR NEW GAME NUMBER
-          var myArr = Array(120).join().split(',').map(function(a){return this.i++},{i:1});
-          var gameNumber = myArr[Math.floor(Math.random () * myArr.length)];
-          
 
-   
-//  GENERATE NEW NUMBER FOR EACH DIAMOND
-          var myArr = Array(12).join().split(',').map(function(a){return this.i++},{i:1});
-          var gemNumber = myArr[Math.floor(Math.random() * myArr.length)];
-    
-    
-    $(".diamond").on("click", function(){
-        $("#getNumber").html(NNEDSOMETHINGINHERE);
+
         
-        if (getNumber === gamenumber){
-            wins++;
-        } if else {
-            (getNumber > gamenumber)
-            losses++;
-        }
-    });
+        
       
+
+          $(".btn").on("click", function(startGame){
+           var myArr = Array(120).join().split(',').map(function(a){return this.i++},{i:1});
+         
+          var gameNumber = myArr[Math.floor(Math.random () * myArr.length)];
+          $(".getNumber").html(gameNumber)
+        alert(gameNumber)
+      });
+              
+////  DO THIS FOR NEW GAME NUMBER
+//          var myArr = Array(120).join().split(',').map(function(a){return this.i++},{i:1});
+//          var gameNumber = myArr[Math.floor(Math.random () * myArr.length)];
+//          
+//
+//   
+////  GENERATE NEW NUMBER FOR EACH DIAMOND
+//          var myArr = Array(12).join().split(',').map(function(a){return this.i++},{i:1});
+//          var gemNumber = myArr[Math.floor(Math.random() * myArr.length)];
+//          
+//          
+//    
+//    var counter = 0;
+//    $(".whiteGem", ".blueGem", ".redGem", ".skyGem").on("click", function(){
+//        $("#getNumber").html(NNEDSOMETHINGINHERE);
+//        
+//        if (getNumber === gamenumber){
+//            wins++;
+//        } if else {
+//            (getNumber >+ gamenumber)
+//            losses++;
+//        }
+//    });
+     }
       
